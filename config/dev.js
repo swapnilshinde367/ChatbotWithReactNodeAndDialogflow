a00ca0d7242fa2b676b844f1d0ee21b68075b57b
XCHANGING.MY\Swapnil.Shinde <swapnil.shinde@asia.xchanging.com>
2018-11-08 07:54:23 +0800

    make it better

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,5 @@ module.exports = {
 	dialogFlowSessionLanguageCode: process.env.LANGUAGE_CODE,
 	googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
 	googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
-	mongoURI: process.env.MONGO_URI
+	mongoURI: process.env.GOOGLE_PRIVATE_KEY
 };
